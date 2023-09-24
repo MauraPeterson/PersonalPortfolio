9b9c8d3cf773565e41e419c3ff216c8d218de709
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-09-24 16:00:58 -0600

    Fixed typo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,9 +6,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/PersonalPortfolio
+DIR="/home/ec2-user/PersonalPortfolio"
 if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
+  echo " ${DIR} exists "
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
