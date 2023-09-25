aab0f51491baaeb129e924d81b355e57295a2351
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-09-25 14:33:09 -0600

    Add styles

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const { readFile, readFileSync } = require('fs');
 
+const path = require('path');
 const app = express();
 
+
 app.get('/', (request, response) => {
-  readFile('./home.html', 'utf8', (err, html) => {
+  readFile('./index.html', 'utf8', (err, html) => {
     
     if(err) {
       response.status(500).send('sorry, out of order')
       console.log(err);
     }
-
+    
     response.send(html);
   })
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
